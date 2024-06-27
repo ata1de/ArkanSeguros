82ac74a259ae51ad335da475285c0bad20c85907
Mateus Ataide <teteulima@outlook.com>
2024-06-27 10:34:09 -0300

    feat/AdminPage: created function to getAll clients

--- src/services/clients.ts
+++ src/services/clients.ts
@@ -1,6 +1,6 @@
 import { db } from "@/lib/firebase";
 import { ClientType } from "@/types/clientType";
-import { addDoc, collection, query } from "firebase/firestore";
+import { addDoc, collection, query, getDoc, getDocs } from "firebase/firestore";
 
 export async function createClient(data: ClientType) {
     const clientRef = collection(db, "clients");
@@ -9,6 +9,20 @@ export async function createClient(data: ClientType) {
     try {
         await addDoc(clientRef, data);
         return { status: 200 }
+    } catch (error) {
+        console.log("Error adding document: ", error)
+        return { status: 400 }
+    }
+}
+
+export async function getAllClients() {
+    const clientRef = collection(db, "clients");
+
+    const q = query(clientRef);
+    try {
+        const clients = await getDocs(clientRef);
+
+        return { status: 200, data: clients }
     } catch (error) {
         console.log("Error adding document: ", error)
         return { status: 400 }
