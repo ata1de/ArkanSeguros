692bfcee34f4c1e1c52123dc0e13bbc4c4e5c368
Mateus Ataide <teteulima@outlook.com>
2025-05-24 10:37:02 -0300

    ft: update leads table

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+import Store from "@/lib/queryClient";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-import Store from "@/lib/queryClient";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -13,8 +13,8 @@ export const metadata: Metadata = {
   title: "Arkan Seguros",
   description: "Proteção e segurança com seguros personalizados para você.",
   icons: {
-    icon: '/icons/arkanWhite.ico',
-  }
+    icon: "/icons/arkanWhite.ico",
+  },
 };
 
 export default function RootLayout({
@@ -23,16 +23,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
-        <Store>
-          {children}
-        </Store>
+        <Store>{children}</Store>
       </body>
     </html>
   );
